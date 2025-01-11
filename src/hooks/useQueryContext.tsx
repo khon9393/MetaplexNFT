4ee02a8676d5d8993e465902042aaa5bea8bde70
Khon Sayakhom <khon9393@hotmail.com>
2025-01-10 19:40:06 -0800

    Prod 1/10/2025

--- src/hooks/useQueryContext.tsx
+++ src/hooks/useQueryContext.tsx
@@ -1,17 +1,8 @@
-import { useRouter } from 'next/router'
-import { EndpointTypes } from '../models/types'
 
 export default function useQueryContext() {
-  const router = useRouter()
-  const { cluster } = router.query
-
-  const endpoint = cluster ? (cluster as EndpointTypes) : 'mainnet'
+  const endpoint ='mainnet'
   const hasClusterOption = endpoint !== 'mainnet'
   const fmtUrlWithCluster = (url) => {
-    if (hasClusterOption) {
-      const mark = url.includes('?') ? '&' : '?'
-      return decodeURIComponent(`${url}${mark}cluster=${endpoint}`)
-    }
     return url
   }
 
