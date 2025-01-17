47d057e503c19fc9072984034f5cfb819feaf5fa
Khon Sayakhom <khon9393@hotmail.com>
2025-01-16 21:47:46 -0800

    Added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
+
+// module.exports = nextConfig
+
+
 /** @type {import('next').NextConfig} */
+const fs = require('fs');
+const path = require('path');
+
+// Load SSL certificates for HTTPS
+const httpsOptions = {
+  key: fs.readFileSync(path.join(__dirname, './certs/key.pem')),
+  cert: fs.readFileSync(path.join(__dirname, './certs/cert.pem')),
+};
+
+// Next.js configuration
 const nextConfig = {
   reactStrictMode: true,
-}
+  server: {
+    https: httpsOptions, // Add HTTPS configuration
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
