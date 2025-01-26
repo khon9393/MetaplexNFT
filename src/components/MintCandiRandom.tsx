86dc49ee7dfd36fc40b365bd731d35f705878eca
Khon Sayakhom <khon9393@hotmail.com>
2025-01-26 09:55:27 -0800

    Added

--- src/components/MintCandiRandom.tsx
+++ src/components/MintCandiRandom.tsx
@@ -67,7 +67,7 @@ export const MintCandiRandom: FC = () => {
                         collection: collectionMint,
                         mintArgs: {
                             solPayment: some({ destination: treasury }),
-                            mintLimit: some({ id: 2 }),
+                            mintLimit: some({ id: 1 }),
                         },
                     })
                 );
