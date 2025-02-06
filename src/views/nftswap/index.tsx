3c985cf67c159006cfe7722ce50d8631333634a0
Khon Sayakhom <khon9393@hotmail.com>
2025-02-06 15:53:19 -0800

    update

--- src/views/nftswap/index.tsx
+++ src/views/nftswap/index.tsx
@@ -13,16 +13,16 @@ export const NFTSwapView: FC = ({ }) => {
   return (
     <div className="md:hero mx-auto p-4">
       <div className="md:hero-content flex flex-col">
-        {/* <h1 className="text-center text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-indigo-500 to-fuchsia-500 mt-10 mb-8 p-2">
+        <h1 className="text-center text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-indigo-500 to-fuchsia-500 mt-10 mb-8 p-2">
           Comming Soon!
-        </h1> */}
+        </h1>
         <div className="text-center">
           {/* <DrawerWindow /> */}
 
-           <UmiProvider>
+           {/* <UmiProvider>
             <SwapWrapper />
             <Toaster />
-          </UmiProvider> 
+          </UmiProvider>  */}
         </div>
     </div>
   </div>
