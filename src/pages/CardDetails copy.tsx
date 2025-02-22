bc51eb35437ed3ee988eaab0f83d66f5f8cb1dad
Khon Sayakhom <khon9393@hotmail.com>
2025-02-21 22:07:50 -0800

    update

--- src/pages/CardDetails copy.tsx
+++ src/pages/CardDetails copy.tsx
@@ -25,8 +25,6 @@ import { motion } from "framer-motion";
 import { NFTStatusTypes } from "@/models/types";
 import { UmiProvider } from "@/providers/umiProvider";
 import { Toaster } from "@/components/ui/toaster";
-// import { UmiProvider } from "@/providers/umiProvider";
-// import { Toaster } from "@/components/ui/toaster";
 
 const CardDetails: FC = () => {
 
@@ -194,7 +192,7 @@ const CardDetails: FC = () => {
                   candyMachines[0]?.collectionStatus === NFTStatusTypes.Available &&
                   (
                     <div>
-           <UmiProvider>
+           {/* <UmiProvider> */}
  
          
          
@@ -206,8 +204,8 @@ const CardDetails: FC = () => {
                         buttonText={candyMachines[0]?.images.length > 1 ? "Mint Random NFT" : ""}
 
                       />
-   <Toaster />
-</UmiProvider> 
+   {/* <Toaster />
+</UmiProvider>  */}
                     </div>
                   )}
               </div>
