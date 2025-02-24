18481a605397e31b1d4396600b0b9de43c17b2a1
Khon Sayakhom <khon9393@hotmail.com>
2025-02-23 18:04:28 -0800

    balance update

--- src/contexts/NetworkConfigurationProvider.tsx
+++ src/contexts/NetworkConfigurationProvider.tsx
@@ -1,5 +1,5 @@
 import { useLocalStorage } from '@solana/wallet-adapter-react';
-import { createContext, FC, ReactNode, useContext } from 'react';
+import { createContext, FC, ReactNode, useContext, useState } from 'react';
 const Endpoint_env = process.env.NEXT_PUBLIC_RPC_ENV;
 
 export interface NetworkConfigurationState {
@@ -16,7 +16,8 @@ export function useNetworkConfiguration(): NetworkConfigurationState {
                                                                                 // mainnet-beta
                                                                                 // devnet
 export const NetworkConfigurationProvider: FC<{ children: ReactNode }> = ({ children }) => {
-    const [networkConfiguration, setNetworkConfiguration] = useLocalStorage("network", Endpoint_env);
+    //const [networkConfiguration, setNetworkConfiguration] = useLocalStorage("network", Endpoint_env);
+    const [networkConfiguration, setNetworkConfiguration] = useState(Endpoint_env);
 
     return (
         <NetworkConfigurationContext.Provider value={{ networkConfiguration, setNetworkConfiguration }}>{children}</NetworkConfigurationContext.Provider>
