1fed269cf1585f2b070c6075dbe8e2b88ea8ceb6
Khon Sayakhom <khon9393@hotmail.com>
2025-02-25 11:19:24 -0800

    fine tune.

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -4,15 +4,15 @@ import React, { useState, useEffect } from "react";
 import { useAutoConnect } from '../contexts/AutoConnectProvider';
 import NavElement from './nav-element';
 //import { CandiNavigationMenu } from './nav-element/navmenu';
-import {CandibarDropdownMenu1} from './nav-element/nftnavmenu';
+import { CandibarDropdownMenu1 } from './nav-element/nftnavmenu';
 import NetworkSwitcher from './NetworkSwitcher';
 import Image from 'next/image';
-import solanaLogo from '../../public/logos/solanaLogo.png';
+import solanaLogo from '../../public/logos/solana-logo_1.svg';
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import useUserSOLBalanceStore from "../stores/useUserSOLBalanceStore";
 import ThemeSwitcher from "./themeSwitcher";
-import fetchUserTokenAccount from "../lib/fetchUserTokenAccount";
 import TokenBalance from "./tokenBalance";
+import { UmiProvider } from "@/providers/umiProvider";
 
 const WalletMultiButtonDynamic = dynamic(
   async () => (await import('@solana/wallet-adapter-react-ui')).WalletMultiButton,
@@ -21,12 +21,10 @@ const WalletMultiButtonDynamic = dynamic(
 
 export const AppBar: React.FC = () => {
   const { autoConnect, setAutoConnect } = useAutoConnect();
-  //const { connection } = useConnection();
   const [isNavOpen, setIsNavOpen] = useState(false);
   const wallet = useWallet();
   const [balance, setBalance] = useState(useUserSOLBalanceStore((s) => s.balance));
   const { getUserSOLBalance } = useUserSOLBalanceStore()
-  const [candibarbalance, setCandibarbalance] = useState();
 
   useEffect(() => {
     const fetchBalance = async () => {
@@ -49,7 +47,7 @@ export const AppBar: React.FC = () => {
 
       <div className="navbar flex h-30 flex-row md:mb-2 shadow-lg bg-black text-neutral-content border-b border-zinc-600 bg-opacity-66">
         <h1 className="text-center text-3xl md:pl-5 font-bold text-transparent bg-clip-text bg-gradient-to-br from-indigo-500 to-fuchsia-500 mb-4">
-        <label
+          <label
             htmlFor="my-drawer"
             className="btn-gh items-center justify-between md:hidden mr-6"
             onClick={() => setIsNavOpen(!isNavOpen)} // Optional, but you can still toggle the drawer visibility if needed
@@ -74,38 +72,65 @@ export const AppBar: React.FC = () => {
         {/* Wallet & Settings */}
         <div className="navbar-end">
           <div className="hidden md:inline-flex items-center justify-center gap-2">
-            <NavElement
+            {/* <NavElement
               label="Home"
               href="/"
+            /> */}
+            <NavElement
+              label="Getting&nbsp;Started"
+              href="/getstarted"
             />
             <div>
 
-            < CandibarDropdownMenu1 />
-
+              < CandibarDropdownMenu1 />
             </div>
-
-             {/* <div className="flex flex-col items-center gap-2 pt-5" style={{ width: '180px' }}>
-              <WalletMultiButtonDynamic className="btn-ghost btn-sm rounded-btn text-lg mb-2" />
-                  <div className="flex items-center gap-2 ml-0">
-                  <Image src={solanaLogo} alt="Solana Logo" width={80} />
-                  <span className="text-sm">SOL: {(wallet.connected ? balance : 0).toLocaleString()}</span>
-                  </div>
-                  <div className="flex items-center gap-2 ml-0">
-                  <TokenBalance />
-                  </div>
-            </div>  */}
-
             <div className="flex flex-col items-center gap-2 pt-5" style={{ width: '180px' }}>
               <WalletMultiButtonDynamic className="btn-ghost btn-sm rounded-btn text-lg mb-2" />
-              <div className="flex items-center gap-2">
-                <Image src={solanaLogo} alt="Solana Logo" width={80} />
-                <span className="text-sm">SOL: {(wallet.connected ? balance : 0).toLocaleString()}</span>
-              </div>
             </div>
-
           </div>
         </div>
       </div>
+
+      {typeof window !== 'undefined' 
+      && window.location.pathname === '/AstrologyZodiac'
+      && (
+        <div className='border-gray-300 bg-gray-800 text-center min-w-[400px]'>
+          <div className="text-white px-2 font-mono text-1xl md:text-2xl font-bold animate-pulse hover:animate-none">
+        <Link href="/Candi" className="hover:underline hover:text-blue-600">
+          Get NFTs tradable for Candibar Tokens!
+        </Link>
+          </div>
+        </div>
+      )}
+
+
+{!wallet.connected && (
+          <div className='border-gray-300 bg-gray-800 text-center w-full min-w-[400px]'>
+          <div className="text-white px-2 font-mono text-sm md:text-lg lg:text-xl xl:text-2xl animate-pulse whitespace-nowrap overflow-hidden text-ellipsis">
+            Please connect your wallet
+          </div>
+          </div>
+       )}
+
+      <UmiProvider>
+
+        
+        {wallet.connected && (
+          <div className="flex justify-center mx-auto text-center text-white">
+            <div className="flex items-center gap-2 justify-center font-mono text-sm md:text-lg lg:text-xl xl:text-2xl whitespace-nowrap overflow-hidden text-ellipsis">
+              <Image src={solanaLogo} alt="Solana Logo" width={15} height={15} />
+              <span>{(wallet.connected ? balance : 0).toLocaleString()} | </span>
+              <TokenBalance />
+            </div>
+          </div>
+        )}
+
+      </UmiProvider>
+
+
     </div>
+
+
+
   );
 };
--- src/views/getstarted/index.tsx
+++ src/views/getstarted/index.tsx
@@ -6,6 +6,7 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion"
+import Link from "next/link";
 export const GetStartedView: FC = ({ }) => {
   return (
     <div className="md:hero mx-auto p-4">
@@ -13,8 +14,12 @@ export const GetStartedView: FC = ({ }) => {
         {/* Header */}
         <div className="py-6">
           <div className="container mx-auto text-center">
-            <h1 className="text-4xl font-bold">Getting Started with Solana Wallet</h1>
-            <p className="mt-2 text-lg">
+            <h1 
+             className="text-center text-4xl p-4 md:pl-12 font-bold text-transparent bg-clip-text bg-gradient-to-br from-red-400 via-yellow-400 to-blue-500 mb-4"
+            // className="text-4xl font-bold"
+            
+            >Getting Started with Solana Wallet</h1>
+            <p className="mt-2 text-lg font-bold text-transparent bg-clip-text bg-gradient-to-br from-red-400 via-yellow-400 to-blue-500 mb-4">
               Follow these simple steps to embark on your journey with Solana and NFTs.
             </p>
           </div>
@@ -23,8 +28,8 @@ export const GetStartedView: FC = ({ }) => {
         {/* Main Content */}
         <main className="container mx-auto px-6 py-10 ">
           <article className="prose prose-invert prose-lg max-w-3xl mx-auto">
-            <h2 className="text-3xl font-bold mb-6">Getting Started Guide</h2>
-            <Accordion type="single" defaultValue="step1">
+            <h2 className="text-3xl font-bold mb-6 font-bold text-transparent bg-clip-text bg-gradient-to-br from-lime-400 to-yellow-500 mb-4">Getting Setup with a Wallet</h2>
+            <Accordion type="single" collapsible={true} defaultValue="step1">
               <AccordionItem value="step1">
                 <AccordionTrigger className="text-xl font-bold">Step 1: Get a Wallet</AccordionTrigger>
                 <AccordionContent>
@@ -98,16 +103,6 @@ export const GetStartedView: FC = ({ }) => {
                         Kraken
                       </a>
                     </li>
-                    <li>
-                      <a
-                        href="https://ftx.com/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="text-blue-300 hover:underline"
-                      >
-                        FTX
-                      </a>
-                    </li>
                   </ul>
                   <p className="mt-4 text-xl">
                   lternatively, you can purchase SOL directly within the Solflare Wallet using the Onramper Widget by following these steps:
@@ -125,12 +120,71 @@ export const GetStartedView: FC = ({ }) => {
                 </AccordionContent>
               </AccordionItem>
 
-              <AccordionItem value="step3">
+              {/* <AccordionItem value="step3">
                 <AccordionTrigger className="text-xl font-bold">Step 3: Start Exploring</AccordionTrigger>
                 <AccordionContent>
                   <p className="text-xl">
                     Now that your wallet is set up and funded, you&apos;re ready to return to the home page and connect your wallet to claim your Candibar Collection NFTs. Start your journey and secure your piece of this exclusive collection today!
                   </p>
+                </AccordionContent>
+              </AccordionItem> */}
+            </Accordion>
+          </article>
+
+            <article className="prose prose-invert prose-lg max-w-3xl mx-auto py-10 mt-10">
+            <h2 className="text-3xl font-bold mb-6 font-bold text-transparent bg-clip-text bg-gradient-to-br from-lime-400 to-yellow-500 mb-4">Platform Guide Launch Pad</h2>
+            <Accordion type="single" collapsible={true} defaultValue="step1">
+              <AccordionItem value="step1">
+                <AccordionTrigger className="text-xl font-bold">Step 1: Get Candi NFTs</AccordionTrigger>
+                <AccordionContent>
+                  <p className="text-xl">
+                    Candi NFTs are essential for accessing the Candibar platform. 
+                    They grant you entry to exclusive features, rewards, and the vibrant Candibar community. 
+                    Additionally, they can be exchanged for official Candibar tokens, which can be used to purchase items in the Candibar marketplace. 
+                    You can mint them directly from our website here:
+                  </p>
+                  <ul className="list-disc pl-5 mt-3 text-xl">
+                    <li>
+                      <Link href="/Candi" className="text-blue-300 hover:underline">
+                        2025 Candi Collection NFTs
+                      </Link>
+                    </li>
+                  </ul>
+                </AccordionContent>
+              </AccordionItem>
+
+              <AccordionItem value="step2">
+                <AccordionTrigger className="text-xl font-bold">Step 2: Swap Candi NFTs for Candibar Tokens</AccordionTrigger>
+                <AccordionContent>
+                  <p className="text-xl">
+                    Once you have your Candi NFTs, you can swap them for Candibar tokens. 
+                    These tokens are used for transactions within the Candibar ecosystem and can be traded on various exchanges in the near future. 
+                    To swap your Candi NFTs for Candibar tokens, follow these steps:
+                  </p>
+                  <ul className="list-disc pl-5 mt-3 text-xl">
+                    <li>Go to the <Link href="/nftswap" className="text-blue-300 hover:underline">Candibar Swap Page</Link>.</li>
+                    <li>Connect your wallet by clicking on the "Select Wallet" button.</li>
+                    <li>Choose the Candi NFTs you want to swap from your wallet.</li>
+                    <li>Confirm the swap transaction to receive your Candibar tokens.</li>
+                  </ul>
+                </AccordionContent>
+              </AccordionItem>
+
+              <AccordionItem value="step3">
+                <AccordionTrigger className="text-xl font-bold">Step 3: Redeem or Swap NFTs in the Marketplace</AccordionTrigger>
+                <AccordionContent>
+                  <p className="text-xl mt-4">
+                    After swapping your Candi NFTs for Candibar tokens, you can redeem them for exclusive items in the Candibar marketplace or swap them for other NFTs. 
+                    The marketplace features a variety of items, including limited-edition NFTs, collectibles, and more. 
+                    Additionally, you can redeem your Candibar tokens for exclusive Candibar collection NFTs, including the Candi-inspired Zodiac collection. 
+                    To redeem or swap your NFTs, follow these steps:
+                  </p>
+                  <ul className="list-disc pl-5 mt-3 text-xl">
+                    <li>Go to the <Link href="/AstrologyZodiac" className="text-blue-300 hover:underline">AstrologyZodiac Page</Link>.</li>
+                    <li>Connect your wallet by clicking on the "Select Wallet" button.</li>
+                    <li>Choose the Zodiac NFTs you want to redeem from your wallet.</li>
+                    <li>Confirm the redemption transaction to receive your exclusive Zodiac NFTs.</li>
+                  </ul>
                 </AccordionContent>
               </AccordionItem>
             </Accordion>
