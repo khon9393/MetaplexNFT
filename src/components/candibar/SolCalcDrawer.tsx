ee281d13265c59e89f60f1ce7e4bde29360b81db
Khon Sayakhom <khon9393@hotmail.com>
2025-03-06 18:32:44 -0800

    Sol Calc

---
+++ src/components/candibar/SolCalcDrawer.tsx
@@ -0,0 +1,68 @@
+
+import { FC } from 'react';
+import * as React from "react";
+import Candibar from "../../../public/2025/CandiBarNoBG.png";
+import Image from 'next/image'
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { Button } from '@/components/ui/button';
+import { ArrowDownTrayIcon, ArrowUpTrayIcon } from "@heroicons/react/24/outline";
+import Soltousdtcalc from './convert';
+import Link from 'next/link';
+
+
+export const SolCalcDrawer: FC = () => {
+
+
+  return (
+
+    <div className="flex flex-row justify-center">
+      <Drawer>
+
+        <DrawerTrigger>
+
+          <Button
+            className="text-white hover:text-blue-600 text-sm md:text-sm "
+            variant="default"
+          >
+            SOL to USD
+            <ArrowUpTrayIcon
+              className='w-15 h-15 m-1'
+            />
+          </Button>
+
+        </DrawerTrigger>
+
+        <DrawerContent className="h-[65vh] bg-gray-400"
+        style={{ backgroundImage: `url(/api/image/CandibarImg/Candi/collection_01-d2uDCK56e8T5KJCGR2aTRayvFYVYZL.jpeg)`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center', opacity: 1 }}
+        >
+          <DrawerHeader
+            //className="absolute left-0 top-0"
+            className="justify-center items-center"
+          >
+            <DrawerClose asChild>
+
+              <ArrowDownTrayIcon
+                className='w-10 h-10 m-1 bg-gray-800 text-grey-900 hover:text-blue-600 cursor-pointer rounded-lg'
+              />
+            </DrawerClose>
+          </DrawerHeader>
+          <Soltousdtcalc />
+          <DrawerFooter>
+           
+          </DrawerFooter>
+
+        </DrawerContent>
+      </Drawer>
+    </div>
+  )
+
+}
