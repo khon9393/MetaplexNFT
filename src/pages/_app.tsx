37af68dc7ae3cd0b1cb76c9f01eff85f7a3bef98
Khon Sayakhom <khon9393@hotmail.com>
2025-03-07 11:06:54 -0800

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,7 @@ import Notifications from '../components/Notification'
 // require('../styles/globals.css');
 import "../styles/globals.css"; // ✅ Recommended
 import "@solana/wallet-adapter-react-ui/styles.css"; // ✅ Recommended
-import FirstVisitModal from '@/components/candibar/FirstVisitModal'; // adjust path as necessary
+import FirstVisitModal from '@/components/candibar/FirstVisitModal'; 
 import appletouch from '../../public/apple-touch-icon.png';
 import { Toaster } from '@/components/ui/toaster';
 import { UmiProvider } from "../providers/umiProvider";
@@ -36,7 +36,7 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
             <meta name="twitter:description" content="Candibar NFTs - Buy, Trade, and Sell Unique Digital Assets | Candibar Tokens Marketplace" />
             <meta name="twitter:image" content="https://www.candibar.com/images/twitter-image.jpg" />
             <meta name="seobility" content="c04242ff42b70e2b459d7520c519e14e" />
-            <link rel="apple-touch-icon" sizes="180x180" href={appletouch.src} />
+            {/* <link rel="apple-touch-icon" sizes="180x180" href={appletouch.src} /> */}
             </Head>
           {/* <FirstVisitModal /> */}
 
