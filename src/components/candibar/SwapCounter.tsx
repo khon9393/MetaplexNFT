0dcdb70162475ac7679e25e16e6b91fbea7a5386
Khon Sayakhom <khon9393@hotmail.com>
2025-03-08 08:54:55 -0800

    update

--- src/components/candibar/SwapCounter.tsx
+++ src/components/candibar/SwapCounter.tsx
@@ -39,52 +39,52 @@ export default function SwapCounter() {
 
   return (
 
-    <div className="flex flex-col md:flex-row w-full h-full mb-6">
+    <div className="">
 
-      <div className="flex-1 p-4">
-        <h2 className="text-white mb-2 text-lg md:text-xl">Current Swap</h2>
+      <div className="flex-1 p-4 w-full md:w-1/2">
+      <h2 className="text-white mb-2 text-lg md:text-xl">Current Swap</h2>
 
-        {Object.entries(zodiacSigns).map(([sign, { icon, dateRange }]) => {
+      {Object.entries(zodiacSigns).map(([sign, { icon, dateRange }]) => {
 
-          const [startMonth, startDay] = dateRange.split(" - ")[0].split(" ");
-          const [endMonth, endDay] = dateRange.split(" - ")[1].split(" ");
-          const startDate = new Date(`${startMonth} ${startDay}, ${serverDate.getFullYear()}`);
-          const endDate = new Date(`${endMonth} ${endDay}, ${serverDate.getFullYear()}`);
+        const [startMonth, startDay] = dateRange.split(" - ")[0].split(" ");
+        const [endMonth, endDay] = dateRange.split(" - ")[1].split(" ");
+        const startDate = new Date(`${startMonth} ${startDay}, ${serverDate.getFullYear()}`);
+        const endDate = new Date(`${endMonth} ${endDay}, ${serverDate.getFullYear()}`);
 
-          const isActive = serverDate >= startDate && serverDate <= endDate;
+        const isActive = serverDate >= startDate && serverDate <= endDate;
 
-          if (isActive) {
-            return (
-              <div key={sign} className="p-4 bg-gray-800 border-2 border-gray-600 rounded-lg">
-                <p className="text-white text-sm md:text-base">{icon} {sign} NFT Collection {dateRange}</p>
-                <p className="text-gray-100 pl-10 text-sm md:text-base"><Countdown endDate={endDate} /></p>
-              </div>
-            );
-          }
-          return null;
+        if (isActive) {
+        return (
+            <div key={sign} className="p-4 bg-gray-800 border-l-2 border-gray-600 rounded-lg">
+          <p className="text-white text-sm md:text-base">{icon} {sign} NFT Collection {dateRange}</p>
+          <p className="text-gray-100 pl-10 text-sm md:text-base"><Countdown endDate={endDate} /></p>
+          </div>
+        );
         }
-        )}
+        return null;
+      }
+      )}
 
       </div>
       
-      <div className="flex-1 p-4 bg-gray-800 border-l-2 border-gray-600 rounded-lg">
-        <h2 className="text-white mb-2 text-lg md:text-xl">Upcoming Swaps</h2>
-        {Object.entries(zodiacSigns)
-          .map(([sign, { icon, dateRange }]) => {
-            const [startMonth, startDay] = dateRange.split(" - ")[0].split(" ");
-            const [endMonth, endDay] = dateRange.split(" - ")[1].split(" ");
-            const startDate = new Date(`${startMonth} ${startDay}, ${serverDate.getFullYear()}`);
-            const endDate = new Date(`${endMonth} ${endDay}, ${serverDate.getFullYear()}`);
+      <div className="flex-1 p-4 bg-gray-800 border-l-2 border-gray-600 rounded-lg w-full md:w-1/2">
+      <h2 className="text-white mb-2 text-lg md:text-xl">Upcoming Swaps</h2>
+      {Object.entries(zodiacSigns)
+        .map(([sign, { icon, dateRange }]) => {
+        const [startMonth, startDay] = dateRange.split(" - ")[0].split(" ");
+        const [endMonth, endDay] = dateRange.split(" - ")[1].split(" ");
+        const startDate = new Date(`${startMonth} ${startDay}, ${serverDate.getFullYear()}`);
+        const endDate = new Date(`${endMonth} ${endDay}, ${serverDate.getFullYear()}`);
 
-            return { sign, icon, dateRange, startDate, endDate };
-          })
-          .filter(({ startDate }) => startDate > serverDate)
-          .sort((a, b) => a.startDate.getTime() - b.startDate.getTime())
-          .map(({ sign, icon, dateRange }, index) => (
-            <div key={sign} className="p-0 items-start flex justify-start mb-2">
-                <p className={`text-sm md:text-base ${index === 0 ? 'text-white' : 'text-gray-400'}`}>{icon} {sign} NFT Collection {dateRange}</p>
-            </div>
-          ))}
+        return { sign, icon, dateRange, startDate, endDate };
+        })
+        .filter(({ startDate }) => startDate > serverDate)
+        .sort((a, b) => a.startDate.getTime() - b.startDate.getTime())
+        .map(({ sign, icon, dateRange }, index) => (
+        <div key={sign} className="p-0 items-start flex justify-start mb-2">
+          <p className={`text-sm md:text-base ${index === 0 ? 'text-white' : 'text-gray-500'}`}>{icon} {sign} NFT Collection {dateRange}</p>
+        </div>
+        ))}
       </div>
     </div>
   );
