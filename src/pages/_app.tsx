473d84d5f2779a16080914ec43a77e059b33af74
Khon Sayakhom <khon9393@hotmail.com>
2025-03-14 11:59:35 -0700

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -22,18 +22,18 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
         <>
             <Head>
             <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <title>Candibar NFTs - Buy, Trade, Sell, NFT, Candibar Tokens</title>
-            <meta name="description" content="Candibar NFTs - Buy, Trade, and Sell Unique Digital Assets. Candibar Tokens Marketplace." />
-            <meta name="keywords" content="Candibar, NFTs, Buy, Trade, Sell, Tokens, Digital Assets, Collectibles, Digital Art" />
+            <title>Candibar NFTs - Buy, Trade, NFT, Candibar Tokens</title>
+            <meta name="description" content="Candibar NFTs - Buy & Trade Unique Digital Assets. Candibar Tokens Marketplace." />
+            <meta name="keywords" content="Candibar, NFTs, Buy, Trade, Tokens, Digital Assets, Collectibles, Digital Art" />
             <meta name="author" content="Candibar" />
             <meta property="og:title" content="Candibar NFTs" />
-            <meta property="og:description" content="Candibar NFTs - Buy, Trade, and Sell Unique Digital Assets | Candibar Tokens Marketplace" />
+            <meta property="og:description" content="Candibar NFTs - Buy, and Trade Unique Digital Assets | Candibar Tokens Marketplace" />
             <meta property="og:type" content="website" />
             <meta property="og:url" content="https://www.candibar.com" />
             <meta property="og:image" content="https://www.candibar.com/images/og-image.jpg" />
             <meta name="twitter:card" content="summary_large_image" />
             <meta name="twitter:title" content="Candibar NFTs" />
-            <meta name="twitter:description" content="Candibar NFTs - Buy, Trade, and Sell Unique Digital Assets | Candibar Tokens Marketplace" />
+            <meta name="twitter:description" content="Candibar NFTs - Buy, and Trade Unique Digital Assets | Candibar Tokens Marketplace" />
             <meta name="twitter:image" content="https://www.candibar.com/images/twitter-image.jpg" />
             <meta name="seobility" content="c04242ff42b70e2b459d7520c519e14e" />
             {/* <link rel="apple-touch-icon" sizes="180x180" href={appletouch.src} /> */}
